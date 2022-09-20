1e469020a53efa2d14885b8ab37093984a92a5c3
sabri <mohamed.baazaoui@hp.com>
2022-09-20 22:01:29 +0100

    create classes && implemnt functions&&implement test

---
+++ DistrubBoissonChaud.Test/BoissonCostTest.cs
@@ -0,0 +1,114 @@
+
+
+using DistrubBoissonChaud;
+
+namespace TestFunctions
+{
+    [TestFixture]
+    public class BoissonCostTest
+    {
+        // cost of the Expresso recipe
+        [Test]
+        public void CalculCost_Expresso_boisson()
+        {
+            Recette rec = new Recette();
+            rec.Name = "expresso";
+            rec.Cafe = 1;
+            rec.Eau = 1;
+            var res = BoissonCost.CalculCostBoission(rec);
+            string expect = "Le prix de recette de expresso est 1.2 et le prix de Vente est 1.56";
+            Assert.That(res, Is.EqualTo(expect));
+        }
+        // cost of the allongé recipe 
+        [Test]
+        public void CalculCost_allongé_boisson()
+        {
+            Recette rec = new Recette();
+            rec.Name = "allongé";
+            rec.Cafe = 1;
+            rec.Eau = 2;
+            var res = BoissonCost.CalculCostBoission(rec);
+            string expect = "Le prix de recette de allongé est 1.4 et le prix de Vente est 1.8199999999999998";
+            Assert.That(res, Is.EqualTo(expect));
+        }
+        // cost of the allonge recipe when you forget to put é
+        [Test]
+        public void CalculCost_allonge_boisson()
+        {
+            Recette rec = new Recette();
+            rec.Name = "allonge";
+            rec.Cafe = 1;
+            rec.Eau = 2;
+            var res = BoissonCost.CalculCostBoission(rec);
+            string expect = "Le prix de recette de allonge est 1.4 et le prix de Vente est 1.8199999999999998";
+            Assert.That(res, Is.EqualTo(expect));
+        }
+        // cost of the capuccino recipe 
+        [Test]
+        public void CalculCost_capuccino_boisson()
+        {
+            Recette rec = new Recette();
+            rec.Name = "capuccino";
+            rec.Cafe = 1;
+            rec.Choclat = 1;
+            rec.Eau = 1;
+            rec.Creme = 1;
+            var res = BoissonCost.CalculCostBoission(rec);
+            string expect = "Le prix de recette de capuccino est 2.7 et le prix de Vente est 3.5100000000000002";
+            Assert.That(res, Is.EqualTo(expect));
+        }
+        // cost of the chocolat recipe 
+
+        [Test]
+        public void CalculCost_Chocolat_boisson()
+        {
+            Recette rec = new Recette();
+            rec.Name = "chocolat";
+            rec.Lait = 2;
+            rec.Choclat = 3;
+            rec.Eau = 1;
+            rec.Sucre = 1;
+            var res = BoissonCost.CalculCostBoission(rec);
+            string expect = "Le prix de recette de chocolat est 4.1 et le prix de Vente est 5.329999999999999";
+            Assert.That(res, Is.EqualTo(expect));
+        }
+        // cost of the thé recipe
+
+        [Test]
+        public void CalculCost_thé_boisson()
+        {
+            Recette rec = new Recette();
+            rec.Name = "thé";
+            rec.The = 1;
+            rec.Eau = 2;
+            var res = BoissonCost.CalculCostBoission(rec);
+            string expect = "Le prix de recette de thé est 2.4 et le prix de Vente est 3.12";
+            Assert.That(res, Is.EqualTo(expect));
+        }
+        // cost of the thé recipe  when you forget to Put é
+        [Test]
+        public void CalculCost_the_boisson()
+        {
+            Recette rec = new Recette();
+            rec.Name = "the";
+            rec.The = 1;
+            rec.Eau = 2;
+            var res = BoissonCost.CalculCostBoission(rec);
+            string expect = "Le prix de recette de the est 2.4 et le prix de Vente est 3.12";
+            Assert.That(res, Is.EqualTo(expect));
+        }
+        [Test]
+        public void CalculCost_autre_boisson()
+        {
+            Recette rec = new Recette();
+            rec.Name = "autre";
+            rec.The = 1;
+            rec.Eau = 2;
+            rec.Choclat = 0;
+            rec.Creme = 0;
+            var res = BoissonCost.CalculCostBoission(rec);
+            string expect = "Le prix de recette de autre est 2.4 et le prix de Vente est 3.12";
+            Assert.That(res, Is.EqualTo(expect));
+        }
+    }
+}
--- DistrubBoissonChaud/Program.cs
+++ DistrubBoissonChaud/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DistrubBoissonChaud;
+
+
+// create Menu list for user to choose between them
+Console.WriteLine("Veillez choisir L'un de bossion suivants pour savoir prix de recette et coût");
+Console.WriteLine("1-Expresso");
+Console.WriteLine("2-Allengé");
+Console.WriteLine("3-Capuccino ");
+Console.WriteLine("4-Choclat ");
+Console.WriteLine("5-Thé");
+Console.WriteLine("6-Autre (nouvelle recette)");
+Recette recette = new Recette();
+var menu = new List<string>() { "expresso", "allongé", "allonge", "capuccino", "chocolat", "thé", "the", "autre" };
+bool valid = false;
+// loop till is valid name 
+while (!valid)
+{
+    var input = Console.ReadLine().ToLower();
+    if (BoissonCost.IsExistInListAndValidBoisson(menu, input))
+    {
+        recette.Name = input;
+        valid = true;
+    }
+    else if (BoissonCost.IsNewBoisson(menu, input))
+    {
+        recette.Name = input;
+        bool CheckValidNumber = false;
+        while (!CheckValidNumber)
+        {
+            Console.WriteLine("entrer les doses de Café");
+            CheckValidNumber = int.TryParse(Console.ReadLine(), out int cafe);
+            if (CheckValidNumber) { recette.Cafe = cafe; }
+            Console.WriteLine("entrer les doses de lait");
+            CheckValidNumber = int.TryParse(Console.ReadLine(), out int lait);
+            if (CheckValidNumber) { recette.Lait = lait; }
+            Console.WriteLine("entrer les doses de Thé");
+            CheckValidNumber = int.TryParse(Console.ReadLine(), out int the);
+            if (CheckValidNumber) { recette.The = the; }
+            Console.WriteLine("entrer les doses d'Eau");
+            CheckValidNumber = int.TryParse(Console.ReadLine(), out int eau);
+            if (CheckValidNumber) { recette.Eau = eau; }
+            Console.WriteLine("entrer les doses de Chocolat");
+            CheckValidNumber = int.TryParse(Console.ReadLine(), out int chocalat);
+            if (CheckValidNumber) { recette.Choclat = chocalat; }
+            Console.WriteLine("entrer les doses de Crème");
+            CheckValidNumber = int.TryParse(Console.ReadLine(), out int creme);
+            if (CheckValidNumber) { recette.Creme = creme; }
+            Console.WriteLine("entrer les doses de Sucre");
+            CheckValidNumber = int.TryParse(Console.ReadLine(), out int sucre);
+            if (CheckValidNumber) { recette.Sucre = sucre; }
+
+        }
+        valid = true;
+    }
+    else
+    {
+        Console.WriteLine("le nom n'est pas valide.Essayer encore!");
+    }
+}
+
+
+// call function to Calculate the cost and get the end result 
+Console.WriteLine(BoissonCost.CalculCostBoission(recette));
